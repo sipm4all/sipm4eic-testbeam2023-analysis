d1e7bf4c627751163a3ba925091eaa7a11cfbc41
Nicola Rubini <nicola.rubini@cern.ch>
2023-11-05 07:59:56 +0100

    Comments adrressing and fit improving

--- macros/finecalib_step0.C
+++ macros/finecalib_step0.C
@@ -2,13 +2,11 @@ std::vector<int> devices_indices = {192, 193, 194, 195, 196, 197, 198, 207};
 
 TF1 *f_fit = new TF1("f_fit", "[0]*(1./(exp((x-[1])/[2])+1))*(1./(exp(([3]-x)/[4])+1))", 10., 200.);
 
-void finecalib_step0(std::string input_filename, std::string output_filename = "finetune_calib0.root", int reference_bin_for_distribution_heigth = 70, int minimum_entries = 1)
+void finecalib_step0(std::string input_filename, std::string output_filename = "finecalib_step0.root", int minimum_entries = 1)
 {
-  // Output
-  std::map<std::string, TH1F *> _TH1F;
-
   //  Open file with TDC fine distributions
   TFile *input_file = new TFile(input_filename.c_str());
+  TFile *outfile = new TFile(output_filename.c_str(), "RECREATE");
   //  Loop over available TDC distributions
   for (auto current_device_index : devices_indices)
   {
@@ -16,8 +14,8 @@ void finecalib_step0(std::string input_filename, std::string output_filename = "
     if (!current_calib_histo)
       continue;
     cout << "[INFO] Starting device " << current_device_index << endl;
-    _TH1F[Form("hIF_%i", current_device_index)] = new TH1F(Form("hIF_%i", current_device_index), "hIF", 768, 0, 768);
-    _TH1F[Form("hCUT_%i", current_device_index)] = new TH1F(Form("hCUT_%i", current_device_index), "hCUT", 768, 0, 768);
+    TH1F *hIF = new TH1F(Form("hIF_%i", current_device_index), "hIF", 768, 0, 768);
+    TH1F *hCUT = new TH1F(Form("hCUT_%i", current_device_index), "hCUT", 768, 0, 768);
     //  Take 2D TDC distribution for fit
     for (auto iBin = 1; iBin <= current_calib_histo->GetNbinsX(); iBin++)
     {
@@ -31,18 +29,41 @@ void finecalib_step0(std::string input_filename, std::string output_filename = "
       if (current_histo->GetEntries() < minimum_entries)
       {
         //  Assign dummy values in the calibration object
-        _TH1F[Form("hIF_%i", current_device_index)]->SetBinContent(iBin, IF);
-        _TH1F[Form("hCUT_%i", current_device_index)]->SetBinContent(iBin, CUT);
+        hIF->SetBinContent(iBin, IF);
+        hCUT->SetBinContent(iBin, CUT);
         continue;
       }
       //  Setup fit function
-      f_fit->SetParameter(0, current_histo->GetBinContent(reference_bin_for_distribution_heigth));
-      f_fit->SetParameter(1, 100);
-      f_fit->SetParLimits(1, 80, 120);
+      auto height_guess = current_histo->GetBinContent(current_histo->GetMaximumBin());
+      auto bin_step = 3;
+      auto critical_value = 0.25 * height_guess;
+      auto min_guess = 0.;
+      for (int jBin = 1; jBin <= current_histo->GetNbinsX() - bin_step; jBin += bin_step)
+      {
+        double y1 = current_histo->GetBinContent(jBin);
+        double y2 = current_histo->GetBinContent(jBin + bin_step);
+        if (fabs(y1 - y2) > critical_value)
+        {
+          min_guess = (jBin + bin_step * 0.5);
+        }
+      }
+      auto max_guess = 0.;
+      for (int jBin = current_histo->GetNbinsX(); jBin >= bin_step; jBin -= bin_step)
+      {
+        double y1 = current_histo->GetBinContent(jBin);
+        double y2 = current_histo->GetBinContent(jBin - bin_step);
+        if (fabs(y1 - y2) > critical_value)
+        {
+          max_guess = (jBin - bin_step * 0.5);
+        }
+      }
+      f_fit->SetParameter(0, height_guess);
+      f_fit->SetParameter(1, max_guess);
+      f_fit->SetParLimits(1, max_guess*0.8, max_guess*1.2);
       f_fit->SetParameter(2, 0.5);
       f_fit->SetParLimits(2, 0.1, 1.);
-      f_fit->SetParameter(3, 35);
-      f_fit->SetParLimits(3, 15, 55);
+      f_fit->SetParameter(3, min_guess);
+      f_fit->SetParLimits(3, min_guess*0.8, min_guess*1.2);
       f_fit->SetParameter(4, 0.5);
       f_fit->SetParLimits(4, 0.1, 1.);
       //  Fit Fine distribution
@@ -54,14 +75,12 @@ void finecalib_step0(std::string input_filename, std::string output_filename = "
       IF = maximum - minimum;
       CUT = 0.5 * (minimum + maximum);
       //  Assign the values in the calibration object
-      _TH1F[Form("hIF_%i", current_device_index)]->SetBinContent(iBin, IF);
-      _TH1F[Form("hCUT_%i", current_device_index)]->SetBinContent(iBin, CUT);
+      hIF->SetBinContent(iBin, IF);
+      hCUT->SetBinContent(iBin, CUT);
     }
-  }
-  TFile *outfile = new TFile(output_filename.c_str(), "RECREATE");
-  for (auto [key, object] : _TH1F)
-  {
-    object->Write();
+    outfile->cd();
+    hIF->Write();
+    hCUT->Write();
   }
   outfile->Close();
 }
