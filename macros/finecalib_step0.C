b492592da81461291f6ecd21fd706c912097321c
Nicola Rubini <nicola.rubini@cern.ch>
2023-11-07 09:29:32 +0100

    Comment addressing

--- macros/finecalib_step0.C
+++ macros/finecalib_step0.C
@@ -24,48 +24,26 @@ void finecalib_step0(std::string input_filename, std::string output_filename = "
       //  Book results variable
       auto IF = 0.;
       auto CUT = 0.;
-      auto OFFSET = 0.;
       //  If no entries, skip fit
       if (current_histo->GetEntries() < minimum_entries)
       {
-        //  Assign dummy values in the calibration object
-        hIF->SetBinContent(iBin, IF);
-        hCUT->SetBinContent(iBin, CUT);
+        //  Leave 0 as dummy value in the calibration object
         continue;
       }
       //  Setup fit function
       auto height_guess = current_histo->GetBinContent(current_histo->GetMaximumBin());
-      auto bin_step = 3;
       auto critical_value = 0.25 * height_guess;
-      auto min_guess = 0.;
-      for (int jBin = 1; jBin <= current_histo->GetNbinsX() - bin_step; jBin += bin_step)
-      {
-        double y1 = current_histo->GetBinContent(jBin);
-        double y2 = current_histo->GetBinContent(jBin + bin_step);
-        if (fabs(y1 - y2) > critical_value)
-        {
-          min_guess = (jBin + bin_step * 0.5);
-          break;
-        }
-      }
-      auto max_guess = 0.;
-      for (int jBin = current_histo->GetNbinsX(); jBin >= bin_step; jBin -= bin_step)
-      {
-        double y1 = current_histo->GetBinContent(jBin);
-        double y2 = current_histo->GetBinContent(jBin - bin_step);
-        if (fabs(y1 - y2) > critical_value)
-        {
-          max_guess = (jBin - bin_step * 0.5);
-          break;
-        }
-      }
+      auto min_bin = current_histo->FindFirstBinAbove(critical_value);
+      auto min_guess = current_histo->GetBinLowEdge(min_bin);
+      auto max_bin = current_histo->FindLastBinAbove(critical_value);
+      auto max_guess = current_histo->GetBinLowEdge(max_bin);
       f_fit->SetParameter(0, height_guess);
       f_fit->SetParameter(1, min_guess);
-      f_fit->SetParLimits(1, min_guess * 0.8, min_guess * 1.2);
+      f_fit->SetParLimits(1, min_guess - 10, min_guess + 10);
       f_fit->SetParameter(2, 0.5);
       f_fit->SetParLimits(2, 0.1, 1.);
       f_fit->SetParameter(3, max_guess);
-      f_fit->SetParLimits(3, max_guess * 0.8, max_guess * 1.2);
+      f_fit->SetParLimits(3, max_guess - 10, max_guess + 10);
       f_fit->SetParameter(4, 0.5);
       f_fit->SetParLimits(4, 0.1, 1.);
       //  Fit Fine distribution
