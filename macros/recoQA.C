bbdeeb03b09c9017bbbb0f7bad911adee57d6f9e
Roberto Preghenella <preghenella@bo.infn.it>
2024-06-06 23:22:36 +0200

    Revert "Start of recodata QA"
    This reverts commit 38b557ae00302862de5e5c255831fcf073fcb484.

--- macros/recoQA.C
+++
@@ -1,7 +0,0 @@
-#include "../lib/lightio.h"
-#include "../lib/data.h"
-
-void recoQA(std::string input_file = "recodata.root", std::string output_file = "out.root", std::string save_dir = "./images/")
-{
-  
-}
